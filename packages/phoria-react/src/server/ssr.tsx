3fead61879cbe77a9ecd11af1078cdfefa1db556
Chris Meagher <cpmeeg@gmail.com>
2024-12-08 19:38:48 +0000

    Update deps

--- packages/phoria-react/src/server/ssr.tsx
+++ packages/phoria-react/src/server/ssr.tsx
@@ -20,7 +20,6 @@ async function renderToString<P extends PhoriaIslandProps>(island: PhoriaIsland<
 async function renderToStream<P extends PhoriaIslandProps>(island: PhoriaIsland<FunctionComponent>, props?: P) {
 	// `react-dom/server.edge` is used because of https://github.com/facebook/react/issues/26906
 	// Implemented workaround as per https://github.com/redwoodjs/redwood/pull/10284
-	// Also having to use React 19 RC because `react-dom/server.edge` is not exported from 18.3.1
 	return await renderToReadableStream(
 		<StrictMode>
 			<island.component {...props} />
